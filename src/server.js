038c04fbe018edfffe7eb6adaa51e6351286978e
TheLegend27-ger <55186709+TheLegend27-ger@users.noreply.github.com>
2022-03-02 15:44:56 +0100

    npm installs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
 // Constants
+const express = require('express');
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+
 const port = 8080;
 // const pagesRoutes = require('./routes/pagesRoute')
 const app = express();
